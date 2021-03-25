9105b3a0e675ece10e6ad274fa8567f126e180d8
Rodrigo Bussolo <rbussolo91@gmail.com>
2021-03-25 01:39:12 -0400

    Realizado as alterações para o desafio.

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,25 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.headers;
+
+        if (Array.isArray(user_id)) {
+            throw new Error("Not a single user!");
+        }
+
+        const id: string = user_id as string;
+
+        try {
+            const users = this.listAllUsersUseCase.execute({ user_id: id });
+
+            return response.json(users);
+        } catch (error) {
+            return response.status(400).json({ error: error.message });
+        }
+    }
 }
 
 export { ListAllUsersController };
