44ae5ec21929b87e63a96edf9643bf7e3272dafc
Rodrigo Bussolo <rbussolo91@gmail.com>
2021-03-25 20:49:54 -0400

    Adicionado a documentação da API.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggetFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggetFile));
 app.use("/users", usersRoutes);
 
 export { app };
